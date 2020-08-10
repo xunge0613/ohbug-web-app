1487961e6f0db0967eba3b15741a1acf2eb3f420
chenyueban <jasonchan0527@gmail.com>
2020-08-10 13:55:58 +0800

    fix: 修复低分辨率下白屏的问题

---
+++ src/components/others/menuHeaderRender/Content.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Drawer, Typography, Divider, Skeleton, Avatar } from 'antd';
+import { useDispatch, useSelector } from 'umi';
+
+import { RootState, ProjectModelState, OrganizationModelState, history } from '@/interfaces';
+import { Icon, IconButton, SwitchOrganization } from '@/components';
+import { useBoolean } from '@/hooks';
+import { getPlatformLogo } from '@/utils';
+
+import styles from './menuHeaderRender.less';
+
+const Content: React.FC = () => {
+  const dispatch = useDispatch();
+  const [visible, { setTrue, setFalse }] = useBoolean(false);
+  const organization = useSelector<RootState, OrganizationModelState['current']>(
+    (state) => state.organization.current,
+  );
+  const project = useSelector<RootState, ProjectModelState['current']>(
+    (state) => state.project.current,
+  );
+  const projects = useSelector<RootState, ProjectModelState['data']>((state) => state.project.data);
+
+  return (
+    <>
+      <div className={styles.root} onClick={setTrue}>
+        <Icon type="icon-ohbug-menu-line" style={{ fontSize: 22 }} />
+
+        {organization && project && (
+          <div className={styles.current}>
+            <Typography.Text className={styles.organization} ellipsis>
+              {organization.name}
+            </Typography.Text>
+            <Typography.Text className={styles.project}>{project.name}</Typography.Text>
+          </div>
+        )}
+      </div>
+      <Drawer
+        className={styles.drawer}
+        title={
+          <div className={styles.title}>
+            <IconButton icon="icon-ohbug-close-line" onClick={setFalse} />
+            <span>Ohbug</span>
+          </div>
+        }
+        width={360}
+        placement="left"
+        closable={false}
+        visible={visible}
+        onClose={setFalse}
+      >
+        <div className={styles.organizations}>
+          <h3>团队</h3>
+          <SwitchOrganization />
+        </div>
+
+        <Divider />
+
+        <div className={styles.projects}>
+          <h3>项目</h3>
+          <Skeleton loading={!projects}>
+            <div className={styles.list}>
+              {projects.map((item) => {
+                function handleClick() {
+                  dispatch({
+                    type: 'project/setState',
+                    payload: {
+                      current: item,
+                    },
+                  });
+                  history.push(`/issue?project_id=${item.id}`);
+                  setFalse();
+                }
+
+                return (
+                  <div className={styles.item} key={item.id} onClick={handleClick}>
+                    <Avatar className={styles.avatar} src={getPlatformLogo(item.type)} />
+                    {item.name}
+                  </div>
+                );
+              })}
+            </div>
+          </Skeleton>
+        </div>
+      </Drawer>
+    </>
+  );
+};
+
+export default Content;
